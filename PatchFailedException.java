20262e3a6d506fac03f0f066d085ef0c1cd7dc04
karlisjen <karlisjena@gmail.com>
2024-05-14 20:05:47 -0700

    Update PatchFailedException.java

--- PatchFailedException.java
+++ PatchFailedException.java
@@ -1,10 +1,6 @@
 package utils;
 
-/**
- * Thrown whenever a delta cannot be applied as a patch to a given text.
- *
- * @author <a href="mailto:juanco@suigeneris.org">Juanco Anez</a>
- */
+
 public class PatchFailedException extends DiffException {
     
     private static final long serialVersionUID = 1L;
