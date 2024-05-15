814efc71330be4715e5dfa9dbde38e8862721198
karlisjen <karlisjena@gmail.com>
2024-05-14 20:10:22 -0700

    Update DiffUtils.java

--- src/utils/DiffUtils.java
+++ src/utils/DiffUtils.java
@@ -1,19 +1,4 @@
-/*
-   Copyright 2010 Dmitry Naumenko (dm.naumenko@gmail.com)
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
-package difflib;
+package utils;
 
 import difflib.myers.Equalizer;
 import difflib.myers.MyersDiff;
@@ -23,50 +8,17 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Implements the difference and patching engine
- * 
- * @author <a href="dm.naumenko@gmail.com">Dmitry Naumenko</a>
- * @version 0.4.1
- * @param T
- *            The type of the compared elements in the 'lines'.
- */
+
 public class DiffUtils {
 
 	private static Pattern unifiedDiffChunkRe = Pattern
 			.compile("^@@\\s+-(?:(\\d+)(?:,(\\d+))?)\\s+\\+(?:(\\d+)(?:,(\\d+))?)\\s+@@$");
 
-	/**
-	 * Computes the difference between the original and revised list of elements
-	 * with default diff algorithm
-	 * 
-	 * @param original
-	 *            The original text. Must not be {@code null}.
-	 * @param revised
-	 *            The revised text. Must not be {@code null}.
-	 * @return The patch describing the difference between the original and
-	 *         revised sequences. Never {@code null}.
-	 */
+
 	public static <T> Patch<T> diff(List<T> original, List<T> revised) {
 		return DiffUtils.diff(original, revised, new MyersDiff<T>());
 	}
 
-	/**
-	 * Computes the difference between the original and revised list of elements
-	 * with default diff algorithm
-	 * 
-	 * @param original
-	 *            The original text. Must not be {@code null}.
-	 * @param revised
-	 *            The revised text. Must not be {@code null}.
-	 * 
-	 * @param equalizer
-	 *            the equalizer object to replace the default compare algorithm
-	 *            (Object.equals). If {@code null} the default equalizer of the
-	 *            default algorithm is used..
-	 * @return The patch describing the difference between the original and
-	 *         revised sequences. Never {@code null}.
-	 */
 	public static <T> Patch<T> diff(List<T> original, List<T> revised,
 			Equalizer<T> equalizer) {
 		if (equalizer != null) {
